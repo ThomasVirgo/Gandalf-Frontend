0a3025b20376cf144e68b388080758f6e8bd1262
Thomas Virgo <tomcvirgo@gmail.com>
2021-03-13 14:06:10 +0000

    added players to UI

--- src/components/Table.js
+++ src/components/Table.js
@@ -59,13 +59,39 @@ const Table = ({socket,input,host}) => {
 
     return (
         <div className='table-container'>
-            <div className='card1'><Spade value = {'A'}/></div>
-            <div className='card2'><Club value = {'2'}/></div>
-            <div className='card3'><Diamond value = {'9'}/></div>
-            <div className='card4'><Heart value = {'K'}/></div>
-            <div className='deck'><Diamond value = {'N/A'}/></div>
-            <div className='pile'><Heart value = {'A'}/></div>
+            <div id='host-cards' className='myCards'>
+                <div><Spade value = {'A'}/></div>
+                <div><Club value = {'2'}/></div>
+                <div><Diamond value = {'9'}/></div>
+                <div><Heart value = {'K'}/></div> 
+            </div>
 
+            <div id='player2' className='player2'>
+                <div className='rotate-clockwise'><Spade value = {'2'}/></div>
+                <div className='rotate-clockwise'><Spade value = {'3'}/></div>
+                <div className='rotate-clockwise'><Spade value = {'4'}/></div>
+                <div className='rotate-clockwise'><Spade value = {'5'}/></div>
+            </div>
+
+            <div id='player3' className='player3'>
+                <div><Spade value = {'6'}/></div>
+                <div><Spade value = {'7'}/></div>
+                <div><Spade value = {'8'}/></div>
+                <div><Spade value = {'9'}/></div>
+            </div>
+
+            <div id='player4' className='player4'>
+                <div className='rotate-anticlockwise'><Spade value = {'10'}/></div>
+                <div className='rotate-anticlockwise'><Spade value = {'J'}/></div>
+                <div className='rotate-anticlockwise'><Spade value = {'Q'}/></div>
+                <div className='rotate-anticlockwise'><Spade value = {'K'}/></div> 
+            </div>
+
+            <div id='center-cards' className='center-cards'>
+                <div className='deck'><Diamond value = {'N/A'}/></div>
+                <div className='pile'><Heart value = {'A'}/></div>
+            </div>
+            
             <div className='main-message'>
                 {gameStarted===false && host &&
                    <button onClick = {startGame}>Start Game</button> 
