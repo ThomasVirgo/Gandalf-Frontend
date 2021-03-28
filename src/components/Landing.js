bce490375bbdb52435585b6601754881d1d26257
Thomas Virgo <tomcvirgo@gmail.com>
2021-03-28 11:11:37 +0100

    completed landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -24,30 +24,22 @@ const Landing = ({handleChange, handleSubmit, createNewCode}) => {
                         <span>Room Name:</span>
                         <input id='room' name={room} type='text' onChange={handleChange} placeholder='enter room name' required></input>
                     </label>
-                    {/* <div id='form-element1' className='form-element'>
-                        <label htmlFor='nickname1'>Name/Nickname:</label>
-                        <input id='nickname1' name={nickname} type='text' required onChange={handleChange} placeholder='enter a name or nickname'></input>
-                    </div> */}
-                    {/* <div id='form-element2' className='form-element'>
-                        <label htmlFor='code'>Room Name:</label>
-                        <input id='room' name={room} type='text' onChange={handleChange} placeholder='ask the host for the room name and enter here' required></input>
-                    </div> */}
                     <button type='submit' className='btn1'>Join Game</button>
                 </form>
             </div>    
 
             <div className='new-game'>
-                <form id='new-room' onSubmit={createNewCode}>
+                <form id='new-room' onSubmit={createNewCode} className='form-container'>
                     <h3>Create New Game</h3>
-                    <div id='form-element3' className='form-element'>
-                        <label htmlFor='nickname2'>Name/Nickname:</label>
+                    <label htmlFor='form-element3'>
+                        <span>Name/Nickname:</span>
                         <input id='nickname2' name={nickname} type='text' required onChange={handleChange} placeholder='enter a name or nickname'></input>
-                    </div>
-                    <div id='form-element4' className='form-element'>
-                        <label htmlFor='roomName'>Room Name:</label>
+                    </label>
+                    <label htmlFor='form-element2'>
+                        <span>Room Name:</span>
                         <input id='roomName' name={newRoom} type='text' onChange={handleChange} placeholder='enter a new room name' required></input>
-                    </div>
-                    <input type='submit' value='click to create new game' className='btn2'></input>
+                    </label>
+                    <button type='submit' className='btn2'>Create new game</button>
                 </form>
             </div>
             
